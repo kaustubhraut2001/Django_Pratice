4c2a0a597ad1268f7d3f14a05988dd8890e7d8c3
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-07-02 16:49:54 +0530

    added app and tempated for practice

--- practivce/learningapp/models.py
+++ practivce/learningapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class Test(models.Model):
     name = models.CharField(max_length=100)
     email= models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
+    date_added = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name + self.email 
